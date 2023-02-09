b13471355b95e766fb5e9508fd7081ee935fe3c6
Ht <htcvhs971@gmail.com>
2023-02-09 15:54:43 +0100

    Add declare const necessary for changeColor

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
+/**Declaration variables**/
 const colorpicker = document.getElementById('colorpicker');
+//titre
+const title = document.getElementsByTagName('h1');
+//nav ul li (d'après le css c'est le bg ul qui change de couleur)
+const navListes = document.getElementsByTagName('ul');
+//articles
+const articles = document.getElementsByTagName('article');
+//class active
+const active = document.getElementsByClassName('active');
+//badge (d'après le css c'est le bg label qui change de couleur)
+const badge = document.getElementsByClassName('circle');
+console.log(badge);
 
+/* 
 changeColor = (ev) => {
 	const color = ev.target.value;
 	console.log(color);
 	// votre code ici
 };
 
-colorpicker.addEventListener('change', changeColor);
+colorpicker.addEventListener('change', changeColor); */
