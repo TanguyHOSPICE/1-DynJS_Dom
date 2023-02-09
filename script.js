f7225ad4b9ec34266ba7bb5d59394ca9ece79af3
Ht <htcvhs971@gmail.com>
2023-02-09 16:26:18 +0100

    Add modif on the const & loop for the color

--- script.js
+++ script.js
@@ -2,21 +2,40 @@
 const colorpicker = document.getElementById('colorpicker');
 //titre
 const title = document.getElementsByTagName('h1');
-//nav ul li (d'après le css c'est le bg ul qui change de couleur)
+//nav ul li (d'après le css c'est la couleur du a qui change de couleur)
 const navListes = document.getElementsByTagName('ul');
 //articles
 const articles = document.getElementsByTagName('article');
 //class active
 const active = document.getElementsByClassName('active');
 //badge (d'après le css c'est le bg label qui change de couleur)
-const badge = document.getElementsByClassName('circle');
+const badgeLabel = document.getElementsByClassName('circle');
 console.log(badge);
 
-/* 
 changeColor = (ev) => {
 	const color = ev.target.value;
-	console.log(color);
-	// votre code ici
+
+	//boucle ds ul
+	for (let i = 0; i < navListes.length; i++) {
+		//navbar couleur
+		navListes.item(i).style.background = color;
+	}
+	//badge bg couleur
+	for (let i = 0; i < badge.length; i++) {
+		badgeLabel.item(i).style.background = color;
+	}
+	//bordures articles
+	for (let i = 0; i < articles.length; i++) {
+		articles.item(i).style.borderColor = color;
+	}
+	//titre couleur
+	for (let i = 0; i < title.length; i++) {
+		title.item(i).style.color = color;
+	}
+	//liens active text couleur
+	for (let i = 0; i < active.length; i++) {
+		active.item(i).firstChild.style.color = color;
+	}
 };
 
-colorpicker.addEventListener('change', changeColor); */
+colorpicker.addEventListener('change', changeColor);
